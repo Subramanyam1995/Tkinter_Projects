3b6aeb97fd9fd7bda223fe2d67b7d21998ed4bd7
Subramanyam1995 <subramanyam04111995@gmail.com>
2024-05-23 17:44:16 +0530

    23-05-2024

---
+++ Python_Tkinter_Project_5_ComboBoxAndSpianBox.py
@@ -0,0 +1,32 @@
+import tkinter as tk
+from tkinter import ttk
+from datetime import datetime
+
+
+window = tk.Tk()
+
+window.geometry("400x300")
+
+window.title("ComboBoxAndSpainBox")
+
+combo_var = tk.StringVar(value="Selected_None")
+
+ComboBoxs = ttk.Combobox(window, 
+                         values=["Dileep","Sudeep","Subramanyam"],
+                         textvariable=combo_var ,
+                          state="Disable"
+                        )
+
+ComboBoxs.pack()
+
+SpainBoxs_var = tk.StringVar(value="Selected_None")
+
+SpainBoxs = ttk.Spinbox(window, 
+                        values=["Dileep","Sudeep","Subramanyam"],
+                        textvariable=SpainBoxs_var,
+                        state="Disable"
+                        )
+
+SpainBoxs.pack()
+
+window.mainloop()
