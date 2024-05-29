cde10593dd70669b02453e149251bba79afae1a8
Subramanyam1995 <subramanyam04111995@gmail.com>
2024-05-29 23:14:03 +0530

    29-05-2024

--- Testfile.py
+++ Testfile.py
@@ -1,3 +1,23 @@
-from tkinter import tix
-root = tix.Tk()
-root.tk.eval('package require Tix')
+from tkinter import * 
+from tkinter.ttk import *
+from tkinterweb import HtmlFrame
+
+
+
+
+#Create an instance of tkinter frame
+win = Tk()
+
+win.title("NEw")
+
+
+p1 = PhotoImage(file = 'Photos\pngtree-om-trishul-tattoo-logo-design-png-image_6452847.png')  
+
+win.iconphoto(False, p1) 
+
+b = Button(win, text = 'Press Me!') 
+b.pack(side = TOP)
+win.title('iconphoto() method')
+#win.wm_attributes('-toolwindow', 'True')
+win.mainloop()
+
